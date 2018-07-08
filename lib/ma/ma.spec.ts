8fbf25c26e5e23d3e498980a284494cb24a909b3
christopher <cbacken@gmail.com>
2018-07-07 19:10:13 -0700

    chore: changed inputs to observable

--- lib/ma/ma.spec.ts
+++ lib/ma/ma.spec.ts
@@ -1,14 +1,32 @@
-import { expect } from "chai";
+import { Observable, from, of, interval, pipe } from "rxjs";
+import { take, delay } from "rxjs/operators";
 import { List } from "immutable";
+import { expect } from "chai";
 import { ma } from "./ma";
 
 const noop = () => {};
 
-describe("Moving Averages", () => {
-  describe("Simple Moving Average", () => {
-    it("Should return a SMA of [1, 2, 3, 4, 5]", () => {
+// const randPrice = (): number => yield Math.floor(Math.random() * (1000 - 100) + 100) / 100;
+
+function* randPrice() {
+  while (1) yield Math.floor(Math.random() * (1000 - 100) + 100) / 100;
+}
+
+describe("Observable Moving Averages", () => {
+  describe("Observable Simple Moving Average", () => {
+    it("Should return NaN on empty observable", () => {
+      ma(of(), 3).subscribe(
+        x => {
+          expect(x).to.deep.equal(NaN);
+        },
+        noop,
+        noop
+      );
+    });
+
+    it("Should return a Simple Moving Average of [1, 2, 3, 4, 5]", () => {
       const results = List().asMutable();
-      ma(List([1, 2, 3, 4, 5]), 3).subscribe(
+      ma(from([1, 2, 3, 4, 5]), 3).subscribe(
         x => {
           results.push(x);
         },
@@ -19,9 +37,9 @@ describe("Moving Averages", () => {
       );
     });
 
-    it("Should return a SMA of [1,51,2,41,3,31,4,21,5,11,6,1]", () => {
+    it("Should return a Simple Moving Average of [1,51,2,41,3,31,4,21,5,11,6,1]", () => {
       const results = List().asMutable();
-      ma(List([1, 51, 2, 41, 3, 31, 4, 21, 5, 11, 6, 1]), 2).subscribe(
+      ma(from([1, 51, 2, 41, 3, 31, 4, 21, 5, 11, 6, 1]), 2).subscribe(
         x => {
           results.push(x);
         },
@@ -41,6 +59,21 @@ describe("Moving Averages", () => {
             8.5,
             3.5
           ]);
+        }
+      );
+    });
+
+    it("Should return Simple Moving Average on Timed Sequence", () => {
+      // const source = from(randPrice()).pipe(take(25));
+      const source = interval(50).pipe(take(10));
+      let t = 0;
+      ma(source, 5).subscribe(
+        x => {
+          t = x;
+        },
+        noop,
+        () => {
+          expect(t).to.equal(7);
         }
       );
     });
--- lib/ma/ma.ts
+++ lib/ma/ma.ts
@@ -2,25 +2,39 @@ import { Observable } from "rxjs";
 import { List } from "immutable";
 import { round } from "../common/utils";
 
-export function ma(series: List<number>, period: number = 1) {
+export function ma(obSeries: Observable<number>, period: number = 1) {
   return Observable.create(observer => {
-    let i = 0,
-      s = 0,
-      ma = 0;
-    series.map(d => {
-      s += isNaN(d) ? 0 : d;
-      if (i < period - 1) {
-        ma = NaN;
-      } else if (i + 1 === period) {
-        ma = round(s / period);
-      } else {
-        s -= isNaN(series.get(i - period)) ? 0 : series.get(i - period);
-        ma = round(s / period);
+    let series = List();
+
+    let i: number = 0,
+      s: number = 0,
+      ma: number = 0;
+
+    obSeries.subscribe(
+      d => {
+        series = series.push(d);
+
+        s += isNaN(d) ? 0 : d;
+        if (i < period - 1) {
+          ma = NaN;
+        } else if (i + 1 === period) {
+          ma = round(s / period);
+        } else {
+          s -= isNaN(series.get(i - period) as number)
+            ? 0
+            : (series.get(i - period) as number);
+          ma = round(s / period);
+        }
+        // console.log(`d: ${d} s: ${s} sma: ${ma}`);
+        i++;
+        observer.next(ma);
+      },
+      err => {
+        observer.error(err);
+      },
+      () => {
+        observer.complete();
       }
-      // console.log(`d: ${d} s: ${s} sma: ${ma}`);
-      i++;
-      observer.next(ma);
-    });
-    observer.complete();
+    );
   });
 }
